4dc44429f96b67f896562a197312b95d90b63b3f
Siva Basani <sivabasani95@gmail.com>
2026-08-17 13:37:24 -0500

    Initial Spring Boot backend setup

---
+++ src/main/java/com/example/bookmyscreenbackend/BookmyscreenBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookmyscreenbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmyscreenBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookmyscreenBackendApplication.class, args);
+    }
+
+}
