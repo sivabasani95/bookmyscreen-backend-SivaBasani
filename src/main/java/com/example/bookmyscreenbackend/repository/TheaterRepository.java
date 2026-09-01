d1bce83352c72a0ad8dffaf22f1b4634549e2488
Siva Basani <sivabasani95@gmail.com>
2026-09-01 11:32:51 -0500

    Add theater repository and service

--- src/main/java/com/example/bookmyscreenbackend/repository/TheaterRepository.java
+++ src/main/java/com/example/bookmyscreenbackend/repository/TheaterRepository.java
@@ -1,4 +1,13 @@
 package com.example.bookmyscreenbackend.repository;
 
-public class TheaterRepository {
+import com.example.bookmyscreenbackend.model.Theater;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+// Connects Theater with MySQL
+public interface TheaterRepository extends JpaRepository<Theater, Long> {
+    // Find theaters by state, ignoring upper/lower case.
+    List<Theater> findByStateContainingIgnoreCase(String state);
+
+
 }
--- src/main/java/com/example/bookmyscreenbackend/service/TheaterService.java
+++ src/main/java/com/example/bookmyscreenbackend/service/TheaterService.java
@@ -1,4 +1,43 @@
 package com.example.bookmyscreenbackend.service;
 
+import com.example.bookmyscreenbackend.model.Theater;
+import com.example.bookmyscreenbackend.repository.TheaterRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+// THEATER SERVICE
+// Handles theater logic and talks to TheaterRepository.
+
+@Service
 public class TheaterService {
+
+    private final TheaterRepository theaterRepository;
+
+    // Connect service with repository.
+    public TheaterService(TheaterRepository theaterRepository) {
+        this.theaterRepository = theaterRepository;
+    }
+
+    // CREATE - save a new theater.
+    public Theater CreateTheater(Theater theater) {
+        return theaterRepository.save(theater);
+    }
+
+    // READ ALL - Get all theaters.
+    public List<Theater> getAllTheaters() {
+        return theaterRepository.findAll();
+    }
+
+    // READ ONE - Find theater by ID.
+    public Theater getTheaterById(Long id) {
+        return theaterRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Theater not found"));
+    }
+
+    // FILTER - Find theaters by state.
+    public List<Theater> getTheatersByState(String state) {
+        return theaterRepository.findByStateContainingIgnoreCase(state);
+    }
+
+
 }
